1adad5be5a836d9ee0c1d9d75e9b6cd37aac94f4
Brandon Braun <brandon.braun@gmail.com>
2016-06-01 23:34:38 -0400

    fix issue with setting process.env.TEST_PSEUDOMAP in some use cases throwing an error

--- map.js
+++ map.js
@@ -1,8 +1,11 @@
-if (process.env.npm_package_name === 'pseudomap' &&
-    process.env.npm_lifecycle_script === 'test')
-  process.env.TEST_PSEUDOMAP = 'true'
+function mapSupported () {
+  return (
+    process.env.npm_package_name === 'pseudomap' &&
+      process.env.npm_lifecycle_script === 'test'
+  )
+}
 
-if (typeof Map === 'function' && !process.env.TEST_PSEUDOMAP) {
+if (typeof Map === 'function' && !mapSupported() && !process.env.TEST_PSEUDOMAP) {
   module.exports = Map
 } else {
   module.exports = require('./pseudomap')
